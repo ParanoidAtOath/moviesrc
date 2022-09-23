1c16e12e82e7a758edf0e81897f64cf234465455
#imghost <92677594+ParanoidAtOath@users.noreply.github.com>
2022-09-24 01:18:43 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt-get udpate -y && sudo apt-get upgrade -y
 sudo apt-get install curl
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm install webtorrent-cli -g
+sudo npm install webtorrent-cli -g
